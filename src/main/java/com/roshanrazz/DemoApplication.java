d6e82c30fd303d6da9afa452831a471901faedd3
Roshan Sah <roshansah400@gmail.com>
2023-07-04 16:02:51 +0000

    added all files

--- src/main/java/com/roshanrazz/DemoApplication.java
+++ src/main/java/com/roshanrazz/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class DemoApplication {
 
 	public static void main(String[] args) {
