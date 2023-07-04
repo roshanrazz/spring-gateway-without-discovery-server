2a7037279327cfe54a6d77c6bb4d7479da0b06a8
Roshan Sah <roshansah400@gmail.com>
2023-07-04 17:51:35 +0000

    modified application.yml

--- src/main/java/com/roshanrazz/DemoApplication.java
+++ src/main/java/com/roshanrazz/DemoApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin(origins="*")
+//@CrossOrigin(origins="*")
 public class DemoApplication {
 
 	public static void main(String[] args) {
